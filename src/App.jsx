eb5cd6715b484a88143c064c306bf4110e73e68c
Vishal Vansjariya <vishalvansjariya2512@gmail.com>
2023-03-19 04:02:18 +0530

    Add loading state and error state
    Add style for loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,26 @@ import MovieCard from "./components/MovieCard";
 
 const API_URL = "https://omdbapi.com/?apikey=f3ccff70";
 function App() {
-  const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const getMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
-    setMovies(data.Search);
+    setError(null);
+    try {
+      setIsLoading(true);
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+      setMovies(data.Search);
+      setIsLoading(false);
+      if (data.Response === "False") {
+        throw new Error("Movie not found!");
+      }
+    } catch (error) {
+      setError(error);
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -30,15 +43,15 @@ function App() {
         setSearchTerm={setSearchTerm}
         getMovies={getMovies}
       />
-      {movies?.length > 0 ? (
+      {!isLoading && !error && movies?.length > 0 && (
         <div className="movie__container">
           {movies.map((movie) => {
             return <MovieCard key={movie.imdbID} movie={movie} />;
           })}
         </div>
-      ) : (
-        <h2 className="movie__notfound">Movie not found!</h2>
       )}
+      {isLoading && <div className="movie__loading"></div>}
+      {error && <h2 className="movie__notfound">Movie not found!</h2>}
     </div>
   );
 }
